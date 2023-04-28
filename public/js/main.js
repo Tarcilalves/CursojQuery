8f8d38b4174d266e7ce7acf755149fc9db969709
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-04-28 14:15:34 -0300

    Criando objeto com jQuery - tabela

--- public/js/main.js
+++ public/js/main.js
@@ -67,22 +67,45 @@ function inicializaMarcadores() {
 function inserePlacar(){
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Douglas";
-    var numPalavras = $("#contador-palavras").text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a> "
+    var numPalavras = $("#contador-palavras").text();    
 
-    var linha = "<tr>"+
-                    "<th>" + usuario + "</th>" +
-                    "<td>" + numPalavras + "</td>" +
-                    "<td>" + botaoRemover + "</td>" +
-                "</tr>";
+    var linha = novaLinha(usuario,numPalavras); 
+    linha.find(".botao-remover").click(removeLinha); 
 
     corpoTabela.prepend(linha);
 }
 
-$(".botao-remover").click(function(event){
-    event.preventDefault();
+function novaLinha(usuario,palavras){
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(Palavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").attr("href","#").addClass("botao-remover");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    // Icone dentro do <a>
+    link.append(icone);
+
+    // <a> dentro do <td>
+    colunaRemover.append(link);
+
+    // Os três <td> dentro do <tr>
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+
+function removeLinha(event){
+    
+    event.preventDefault(); // previne o comportamento padrão do elemento
     $(this).parent().parent().remove();
-});
+
+}
+
 
 function reiniciaJogo() {
     campo.attr("disabled", false);
