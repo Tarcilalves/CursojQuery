f7d817148dfbe39b2bdf05cb6faacfa91f1c6b0b
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-04-30 12:21:59 -0300

    remova linha na função atualizaPlacar

--- public/js/placar.js
+++ public/js/placar.js
@@ -90,6 +90,7 @@ function atualizaPlacar(){
     $.get("http://localhost:3000/placar",function(data){
         $(data).each(function(){
             var linha = novaLinha(this.usuario, this.pontos);
+            linha.find(".botao-remover").click(removeLinha); 
             $("tbody").append(linha);
         });
     });
