11b282ad338cf86af138c4250b9d2b698f96d62b
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-04-30 01:03:56 -0300

    Enviando dados com ajax

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,16 +1,23 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
+    $("#spinner").toggle();
+
     $.get("http://localhost:3000/frases",trocaFraseAleatoria)
     .fail(function(){
         $("#erro").show();
         setTimeout(function(){
             $("#erro").toggle();
 
-        }, 300);
+        }, 3000);
              
         
        
+    })
+    .always(function(){
+        $("#spinner").toggle();
+
     });
 }
 
@@ -24,4 +31,31 @@ function trocaFraseAleatoria(){
     atualizaTempoInicial(data[numeroAleatorio].tempo);
 
     
+}
+
+function buscaFrase(){
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+    var dados = { id: fraseId};
+
+    $.get("http://localhost:3000/frases", dados,tocaFrase);
+    .fail(function(){
+        $("#erro").show();
+        setTimeout(function(){
+            $("#erro").toggle();
+
+        }, 3000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+
+    });
+
+}
+
+function trocaFrase(data){
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data,tempo);
 }
